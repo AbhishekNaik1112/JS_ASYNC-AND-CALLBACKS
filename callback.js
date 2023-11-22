2da2d4b174380a4e9df2d427ce37e03d2209b7e0
AbhishekNaik1112 <abhisek.naik@kalvium.community>
2023-11-22 15:34:31 +0530

    js

---
+++ callback.js
@@ -0,0 +1,28 @@
+const cookies = [{ name: "Chocolate Cookies" }, { name: "Macaron Cookies" }];
+const newCookie = { name: "Biscotti Cookies" };
+
+// Progression 1: create a function to get all the cookies
+// Progression 2: using setTimeout()
+function getCookies() {
+  setTimeout(() => {
+    let output = "";
+    cookies.forEach((cookie) => {
+      output += `${cookie.name}<br>`;
+      document.body.innerHTML = output;
+    });
+  }, 1000);
+}
+//Progression 3: Create a function to create cookies
+function createCookie(newCookie, callback) {
+  setTimeout(() => {
+    cookies.push(newCookie);
+    callback();
+  }, 2000);
+}
+// Progression 4: calling function
+createCookie(newCookie, getCookies);
+
+//OUTPUT:
+//Chocolate Cookies
+//Macaron Cookies
+//Biscotti Cookies
